0ffac35dd29ba3eadb15a4ebe0a4595f3a9d2398
spanic <sadulin2011@yandex.ru>
2023-08-27 23:30:37 +0600

    migrates to Storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,25 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: [
     '../src/**/*.stories.mdx',
     '../docs/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app'
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-mdx-gfm',
   ],
+
   staticDirs: ['../public'],
-}
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+
+  features: {
+    autoDocs: false,
+  },
+};
