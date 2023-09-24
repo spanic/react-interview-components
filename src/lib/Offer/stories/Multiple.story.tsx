49b599911ffb6d39760e6a5e34addba407d72269
spanic <sadulin2011@yandex.ru>
2023-09-24 18:29:33 +0600

    adds .d.ts file generation for the lib

--- src/lib/Offer/stories/Multiple.story.tsx
+++ src/lib/Offer/stories/Multiple.story.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable fp/no-mutation */
 
 import React from 'react';
+import type {StoryFn} from '@storybook/react';
 import Default, {Template as DefaultTemplate} from './Default.story';
+import {IOfferProps} from '../Offer';
 
-export const Multiple = DefaultTemplate;
+export const Multiple: StoryFn<IOfferProps> = DefaultTemplate;
 
 Multiple.args = {
   data: Default.args?.data,
